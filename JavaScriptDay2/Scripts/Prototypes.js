ab2b7a1114eae640e860cfc32d2dcb72c0c3612b
Eric Siebeneich <eric.siebeneich@gmail.com>
2015-09-09 17:19:57 -0500

    Closures

---
+++ JavaScriptDay2/Scripts/Prototypes.js
@@ -0,0 +1,14 @@
+﻿function Product(name, price) {
+    this._name = name;
+    this._price = price;
+    this.calculateTotalPrice = function() {
+        return this._price + calculateTax.bind(this)();
+    }
+    function calculateTax() {
+        return this._price * 0.08;
+    }
+}
+var product1 = new Product('Milk', 2.33);
+var product2 = new Product('Cheese', 4.33);
+
+console.log(product1.calculateTotalPrice());
---
+++ JavaScriptDay2/Scripts/this.js
@@ -0,0 +1,97 @@
+﻿
+var counter = {
+    _count: 0,
+    toString: function () {
+        return 'counter';
+    },
+    addCount: function () {
+        this._count++;
+        console.log(`this=${this} and _count=${this._count}`);
+    }
+};
+
+counter.addCount();
+
+var cat = {
+    cat: 'cat'
+};
+
+window.setInterval(function () {
+    this._count++;
+    console.log(`this=${this} and _count=${this._count}`);
+}.bind(cat), 1000);
+
+//var dog = {
+//    dog: 'dog'
+//};
+
+//function printThis() {
+//    console.log(this);
+//}
+
+//window.setInterval(printThis.bind(cat), 1000);
+//window.setInterval(printThis.bind(dog), 800);
+
+var module = (function (window) {
+
+    var _defaultTax = 0.08;
+
+    // private methods
+    function _calculatePrice(price) {
+        return price + _calculateTax(price)
+    }
+
+    function _calculateTax(price) {
+        return price * _defaultTax;
+    }
+
+    // exports
+    return {
+        get defaultTax() {
+            return _defaultTax;
+        },
+        set defaultTax(value) {
+            _defaultTax = value;
+        },
+        calculatePrice: _calculatePrice
+    };
+
+})();
+
+console.log(module.calculatePrice(10)); // 10.80
+module.defaultTax = .1;
+console.log(module.calculatePrice(10)); // ??
+
+var message = "hello world";
+// outer function
+function getFunction() {
+
+    // inner function
+    return function () {
+        console.log(`He says "${message}"`);
+    }
+}
+
+var func = getFunction();
+message = "goodbye";
+func();
+
+/*
+function getSetInterval(millis) {
+    return a function that accepts a function as an argument, and calls
+        window.setInterval on that function with millis as the time
+}
+
+window.setInterval(<function>, millis);
+*/
+
+function getSetInterval(seconds) {
+    return function(func) {
+        window.setInterval(func, seconds * 1000);
+    };
+}
+
+var executeEvery5Seconds = getSetInterval(5);
+executeEvery5Seconds(function () {
+    console.log("Hello World");
+});
